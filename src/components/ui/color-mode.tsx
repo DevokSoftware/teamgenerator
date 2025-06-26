005bc9f92d36f13b53812e4c5ec6d7ab646d2238
Leonel Goncalves <99224848+DevokSoftware@users.noreply.github.com>
2025-06-26 20:42:38 +0200

    Update color-mode.tsx

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import type { IconButtonProps } from "@chakra-ui/react";
-import { ClientOnly, IconButton, Skeleton } from "@chakra-ui/react";
 import { ThemeProvider, useTheme } from "next-themes";
 import type { ThemeProviderProps } from "next-themes/dist/types";
-import { forwardRef } from "react";
 import { LuMoon, LuSun } from "react-icons/lu";
 
 export function ColorModeProvider(props: ThemeProviderProps) {
@@ -30,32 +27,3 @@ export function ColorModeIcon() {
   const { colorMode } = useColorMode();
   return colorMode === "light" ? <LuSun /> : <LuMoon />;
 }
-
-interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
-
-export const ColorModeButton = forwardRef<
-  HTMLButtonElement,
-  ColorModeButtonProps
->(function ColorModeButton(props, ref) {
-  const { toggleColorMode } = useColorMode();
-  return (
-    <ClientOnly fallback={<Skeleton boxSize="8" />}>
-      <IconButton
-        onClick={toggleColorMode}
-        variant="ghost"
-        aria-label="Toggle color mode"
-        size="sm"
-        ref={ref}
-        {...props}
-        css={{
-          _icon: {
-            width: "5",
-            height: "5",
-          },
-        }}
-      >
-        <ColorModeIcon />
-      </IconButton>
-    </ClientOnly>
-  );
-});
